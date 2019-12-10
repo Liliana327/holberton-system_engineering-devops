c2fbf96002633bcbb07da5fdb32fdf9840e5f658
Liliana327 <894@holbertonschool.com>
2019-12-10 02:32:40 +0000

    Using Puppet, create a file in /tmp.

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Using Puppet, create a file in /tmp.
-file { '/tmp/holberton':
-  path         => '/tmp/holberton',
-  permission   => '0744',
-  owner        => 'www-data',
-  group        => 'www-data',
-  contains     => 'I love Puppet'
+file {'/tmp/holberton':
+ path        => '/tmp/holberton',
+ permission  => '0744',
+ owner       => 'www-data',
+ group       => 'www-data',
+ contains    => 'I love Puppet'
 }
